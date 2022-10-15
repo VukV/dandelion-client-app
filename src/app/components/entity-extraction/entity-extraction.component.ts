4c8ce91a1f16934ad5b6df2a91f65f1ed98d08ab
VukV <vuk.vukovic.2000@gmail.com>
2022-10-15 18:17:35 +0200

    working on entity extraction

--- src/app/components/entity-extraction/entity-extraction.component.ts
+++ src/app/components/entity-extraction/entity-extraction.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntityExtractionComponent implements OnInit {
 
+  text: string = "";
+  confidence: number = 60;
+  image: boolean = false;
+  abstract: boolean = false;
+  categories: boolean =  false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getConfidenceLabel(): string{
+    return "Confidence: " + this.confidence/100;
+  }
+
+  submitExtraction(){
+    //TODO call service
+  }
 }
