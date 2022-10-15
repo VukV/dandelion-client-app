b89e74b3f2010ec6083871040bbead67eeccfca6
VukV <vuk.vukovic.2000@gmail.com>
2022-10-15 20:06:25 +0200

    history service methods

--- src/app/services/history.service.ts
+++ src/app/services/history.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HistoryItem} from "../model/history-item";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
 
+  private history: HistoryItem[] = [];
+
   constructor() { }
+
+  getHistory(): HistoryItem[]{
+    return this.history;
+  }
+
+  addToHistory(url: string){
+    this.history.push(new HistoryItem(url));
+  }
 }
