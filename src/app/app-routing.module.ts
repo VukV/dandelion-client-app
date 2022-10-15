da6db351e68a63eb4a012cfdbed5fe4820d8324c
VukV <vuk.vukovic.2000@gmail.com>
2022-10-15 14:57:55 +0200

    components and navbar setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EntityExtractionComponent} from "./components/entity-extraction/entity-extraction.component";
+import {TextSimilarityComponent} from "./components/text-similarity/text-similarity.component";
+import {LanguageDetectionComponent} from "./components/language-detection/language-detection.component";
+import {SentimentAnalysisComponent} from "./components/sentiment-analysis/sentiment-analysis.component";
+import {ConfigurationComponent} from "./components/configuration/configuration.component";
+import {HistoryComponent} from "./components/history/history.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "entity-extraction",
+    component: EntityExtractionComponent
+  },
+  {
+    path: "",
+    component: EntityExtractionComponent
+  },
+  {
+    path: "text-similarity",
+    component: TextSimilarityComponent
+  },
+  {
+    path: "language-detection",
+    component: LanguageDetectionComponent
+  },
+  {
+    path: "sentiment-analysis",
+    component: SentimentAnalysisComponent
+  },
+  {
+    path: "configuration",
+    component: ConfigurationComponent
+  },
+  {
+    path: "history",
+    component: HistoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { HistoryComponent } from './components/history/history.component';
+import { EntityExtractionComponent } from './components/entity-extraction/entity-extraction.component';
+import { LanguageDetectionComponent } from './components/language-detection/language-detection.component';
+import { SentimentAnalysisComponent } from './components/sentiment-analysis/sentiment-analysis.component';
+import { TextSimilarityComponent } from './components/text-similarity/text-similarity.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigurationComponent,
+    HistoryComponent,
+    EntityExtractionComponent,
+    LanguageDetectionComponent,
+    SentimentAnalysisComponent,
+    TextSimilarityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
